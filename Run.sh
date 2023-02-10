54778e6ce4f656662d5965d5d42cbe834e19f901
choco30 <47217764+choco30@users.noreply.github.com>
2023-02-10 17:11:56 +0530

    Create Run.sh
    This shell script can be used to directly run the data-flow job from the cloud shell

---
+++ Run.sh
@@ -0,0 +1,1 @@
+python3 /home/gcptest63/sapdataflow/dataflow_sap1.py --runner DataflowRunner --project sandy-376313 --region us-central1 --temp_location  gs://sap_poc/temp    --job_name sapjobload5  --setup_file /home/gcptest63/setup.py --dfBucket "sap_poc"     
