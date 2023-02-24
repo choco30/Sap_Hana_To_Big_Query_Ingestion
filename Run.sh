e2e3c4b12eaff27e4f80a64ed2989c813b8ec9f9
choco30 <47217764+choco30@users.noreply.github.com>
2023-02-24 12:33:46 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,1 +1,1 @@
-python3 /home/gcptest63/sapdataflow/dataflow_sap1.py --runner DataflowRunner --project sandy-376313 --region us-central1 --temp_location  gs://sap_poc/temp    --job_name sapjobload5  --setup_file /home/gcptest63/setup.py --dfBucket "sap_poc"     
+python3 /home/gcptest63/sapdataflow/dataflow_sap1.py --runner DataflowRunner --project project_name --region us-central1 --temp_location gs://bucket_name/temp    --job_name sapjobload5  --setup_file /home/bicket_name/setup.py --dfBucket "bucket_name"     
